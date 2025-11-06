d9b311b6c0cb2c669b3b8bf939bb42687f80ef2a
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-11-06 14:53:12 +0000

    .

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-//Selecionar os elementos das luzes
-const luzVermelha = document.getElementByClassNameById("vermelho");
-const luzAmarelo = document.getElementByClassNameById("amarelo");
-const luzVerde = document.getElementsByClassNameById("verde");
+//Seleciona os elementos das luzes
+
+const luzVermelha = document.getElementById("vermelho");
+const luzAmarelo = document.getElementById("amarelo");
+const luzVerde = document.getElementById("verde");
+
 // Botões
 const btnVermelho = document.getElementById("btnVermelho");
 const btnAmarelo = document.getElementById("btnAmarelo");
 const btnVerde = document.getElementById("btnVerde");
 const btnAuto = document.getElementById("btnAuto");
 const btnParar = document.getElementById("btnParar");
+
 // Variável para o modo automático
 let intervalo;
 
@@ -19,31 +22,39 @@ function limpar(){
 
 function acenderVermelho(){
     limpar();
-    luzVermelha.classList.add("Acesa", "vermelha");
+    luzVermelha.classList.add("acesa","vermelha");
 }
+
 
 function acenderAmarelo(){
     limpar();
-    luzAmarelo.classList.add("Acesa", "amarelo");
+    luzAmarelo.classList.add("acesa","amarelo");
 }
+
 
 function acenderVerde(){
     limpar();
-    luzVerde.classList.add("Acesa", "verde");
+    luzVerde.classList.add("acesa","verde");
 }
 
 function modoAutomatico(){
     limpar();
     let estado =0;
     intervalo = setInterval(()=>{
-        if(estado === 0 ) acenderVermelho();
-        else if (estado === 1) acenderAmarelo();
-        else if (estado === 2) acenderVerde();
-        estado = (estado = 1) % 3;
-   },1000)
+        if(estado === 0) acenderVermelho();
+        else if(estado === 1) acenderAmarelo();
+        else if(estado === 2) acenderVerde();
+        estado = (estado + 1) % 3;
+    }, 1000)
 }
 
 function parar(){
     clearInterval(intervalo);
     limpar();
 }
+
+btnParar.onclick = parar;
+btnAuto.onclick = modoAutomatico;
+btnVerde.onclick = acenderVerde;
+btnAmarelo.onclick = acenderAmarelo;
+btnVermelho.onclick = acenderVermelho;
